9a3b7232c876a03359e0e5a55101304878001368
Karlslanterna <karlvbn@gmail.com>
2026-08-21 05:29:34 -0100

    Allow pre-build typecheck for cinema movie sitemap

--- src/routes/sitemap-cinema-movies[.]xml.ts
+++ src/routes/sitemap-cinema-movies[.]xml.ts
@@ -2,7 +2,10 @@ import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 import { CANONICAL_HOST } from "@/lib/canonical";
 
-export const Route = createFileRoute("/sitemap-cinema-movies.xml")({
+// This route is registered in routeTree.gen.ts by the TanStack Vite plugin
+// during build; CI typechecks once before that generated file is refreshed.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const Route = createFileRoute("/sitemap-cinema-movies.xml" as any)({
   server: {
     handlers: {
       GET: async () => {
